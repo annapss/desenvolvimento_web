136d89f93ec3e55370324b4b27da673b5649e817
annapss <annpss07@gmail.com>
2021-12-02 15:21:59 -0300

    Código que lê e faz a tableacom os dados do arquivo csv do campeonato brasileiro

---
+++ album/dados.php
@@ -0,0 +1,54 @@
+<style>
+    table {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    th {
+        background-color: #2971e6;
+    }
+    tr.verde {
+        background-color: #80d6ed;
+    }
+    tr.amarelo {
+        background-color: #aa7eed;
+    }
+</style>
+<table border="1" width="100%">
+<?php
+    $row = 1;
+    $f = fopen("campeonato-brasileiro-full.csv", "r");
+
+    if($f !== FALSE)
+    {
+        $data = fgetcsv($f, 1000, ";");
+
+        while($data !== FALSE)
+        {
+            $num = count($data);
+            if($row % 2 == 0)
+            {
+                echo "<tr class=verde>";
+            }
+            else
+            {
+                echo "<tr class=amarelo>";
+            }
+
+            foreach($data as $valor)
+            {
+                if($row === 1)
+                {
+                    echo "<th>$valor</th>";
+                }
+                else
+                {
+                    echo "<td>$valor</td>";
+                }
+            }
+            echo "</tr>";
+            $data = fgetcsv($f, 1000, ";");
+            $row++;
+        }
+        fclose($f);
+    }
+?>
+</table>
